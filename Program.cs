769e40be5534380e0265316394570550a153b0b9
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 14:25:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen bir dizi sayıyı boşlukla ayırarak girin:");
+        string input = Console.ReadLine();
+        const int @int = 67;
+        string[] numbers = input.Split(' ');
+        int totalDifference = default(int);
+
+        foreach (string numberStr in numbers)
+        {
+            int number = int.Parse(numberStr);
+
+            if (number < @int)
+                totalDifference += @int - number;
+            else
+            {
+                int difference = number - @int;
+                totalDifference += difference * difference;
+            }
+        }
+
+        Console.WriteLine("Sonuç: " + totalDifference);
+    }
+}
